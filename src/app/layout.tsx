75520330263ff31a320870a2800afc61f671c00b
Pedro Ricardo <70300680+phricardo@users.noreply.github.com>
2024-11-01 12:26:46 -0300

    feat: add metadata configuration for OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,27 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Pedro Ricardo - phricardo.com",
   description: "",
+  openGraph: {
+    url: "https://www.opengraph.xyz/",
+    type: "website",
+    title: "OpenGraph - Preview Social Media Share and Generate Metatags",
+    description: "OpenGraph is the easiest way to preview and generate open graph meta tags for any website.",
+    images: [
+      {
+        url: "https://www.opengraph.xyz/og-opengraph-v2.png",
+        alt: "OpenGraph logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@your_twitter_handle", // opcional
+    title: "OpenGraph - Preview Social Media Share and Generate Metatags",
+    description: "OpenGraph is the easiest way to preview and generate open graph meta tags for any website.",
+    images: [
+      "https://www.opengraph.xyz/og-opengraph-v2.png",
+    ],
+  },
 };
 
 export default function RootLayout({
