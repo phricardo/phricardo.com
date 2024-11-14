3e47116973e2687e0986fe421f0cf4cc6da5dbff
Pedro Ricardo <brpedroricardo@gmail.com>
2024-11-14 16:07:22 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,9 @@
 import type { Metadata } from "next";
+import metadataJson from "./metadata.json";
 import { fonts } from "./fonts";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "Pedro Ricardo - phricardo.com",
-  description: "",
-  openGraph: {
-    url: "https://www.opengraph.xyz/",
-    type: "website",
-    title: "OpenGraph - Preview Social Media Share and Generate Metatags",
-    description: "OpenGraph is the easiest way to preview and generate open graph meta tags for any website.",
-    images: [
-      {
-        url: "https://www.opengraph.xyz/og-opengraph-v2.png",
-        alt: "OpenGraph logo",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@your_twitter_handle", // opcional
-    title: "OpenGraph - Preview Social Media Share and Generate Metatags",
-    description: "OpenGraph is the easiest way to preview and generate open graph meta tags for any website.",
-    images: [
-      "https://www.opengraph.xyz/og-opengraph-v2.png",
-    ],
-  },
-};
+export const metadata: Metadata = metadataJson;
 
 export default function RootLayout({
   children,
