7584d8ce4a50e30921b3348348642886db3b7620
Pedro Ricardo <70300680+phricardo@users.noreply.github.com>
2025-01-16 21:35:20 -0300

    chore: update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -60,15 +60,15 @@ export default function Header({
             <div>
               <h1 className={styles.title}>
                 <Motion
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
+                  initial={{ opacity: 0, y: -20, filter: "blur(10px)" }}
+                  animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
                   transition={{ duration: 0.3, delay: 0.4 }}
                 >
                   depuro.
                 </Motion>
                 <Motion
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
+                  initial={{ opacity: 0, y: -20, filter: "blur(10px)" }}
+                  animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
                   transition={{ duration: 0.3, delay: 0.5 }}
                 >
                   <span>logo existo.</span>
