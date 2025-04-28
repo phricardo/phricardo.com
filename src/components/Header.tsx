22f61ab263da19c55a0905f64644c28c777d22fc
Pedro Ricardo <brpedroricardo@gmail.com>
2025-04-27 22:31:15 -0300

    chore: update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,9 @@ const Header = () => {
   return (
     <header className="bg-github-secondary border-b border-github-border w-full sticky top-0 z-50">
       <div className="max-w-[1100px] mx-auto w-full px-4 md:px-6 py-4 flex items-center justify-between">
-        <div className="flex-shrink-0">
+        <a className="flex-shrink-0" href="/">
           <img src={logoSrc} alt="PhRicardo Logo" className="h-6 w-auto" />
-        </div>
+        </a>
 
         <div className="flex items-center gap-2">
           <LanguageMenu />
